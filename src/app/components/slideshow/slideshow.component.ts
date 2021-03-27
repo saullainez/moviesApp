cea77a5005c7432d5d796dfd7fd53c5fc777f96b
Saúl Laínez <saullainez@hotmail.es>
2021-03-27 10:54:24 -0600

    Swiper
    Slideshow de películas

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/now-playing-response';
+import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
   constructor() { 
     
+  }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+    });
+
   }
 
   ngOnInit(): void {
